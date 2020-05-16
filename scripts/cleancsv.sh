fb659e9d2c540f6e52c4c69bf5e8738d0fb3a38d
Sébastien HUSS <sebastien.huss@gmail.com>
2020-05-16 16:45:08 +0200

    Ajout de tooltips pour les graphs et mise à jour de la données

--- scripts/cleancsv.sh
+++ scripts/cleancsv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-src=data/2020-05-07_deces_quotidiens_departement_csv.csv
+src=data/2020-05-15_deces_quotidiens_departement_csv.csv
 dest=data/deces_quotidiens_departement.csv
 #dos2unix <$src|awk "-F;" 'NR==1{print}!/France/&&!/NA/&&NR>1{$3="\""$3"\"";$4="\""$4"\"";$5="\""$5"\"";$6="\""$6"\"";$7="\""$7"\"";$8="\""$8"\"";print $0}'|sed 's/ /;/g'>$dest
 dos2unix <$src|awk "-F;" '!/France/&&!/NA/'|sed 's/;/,/g'>$dest
--- scripts/fetch_data.sh
+++ scripts/fetch_data.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 
 clean() {
-rm data/*json data/*csv
+rm data/*.json data/*.csv
 }
 # Insee
 insee() {
-IURL="https://www.insee.fr/fr/information/4470857"
+IURL="https://www.insee.fr/fr/statistiques/4487988?contenu=4487854"
 IDATA=$(curl -sL $IURL|awk -v p=0 '/class="bloc fichiers"/{p++}p==1&&/csv\.zip/{print}'|sed 's/.*href="//;s/".*//')
 curl -sL "https://www.insee.fr$IDATA"|bsdtar -C data -xvf -
+}
+
+carte() {
+	if ! [ -f data/departements-version-simplifiee.geojson ];then
+		curl -sL https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements-version-simplifiee.geojson >data/departements-version-simplifiee.geojson
+	fi
 }
 
 # data covid
 dcovid() {
 DURL=https://www.data.gouv.fr/fr/datasets/donnees-cartographiques-concernant-lepidemie-de-covid-19/
 DDATA=$(curl -sL $DURL|awk '/departements-france.zip/&&/json_ld/'|sed 's/<script[^<]*>//;s/<\/script>//'|jq -r '.distribution[1].contentUrl')
+DDATA=https://github.com/kalisio/covid-19/raw/master/departements-france/departements-france.zip
 curl -sL $DDATA|bsdtar -C data -xvf -
 }
 
 clean
 insee
+carte
 dcovid
 ./scripts/aggregate_covid.js
 ./scripts/cleancsv.sh
